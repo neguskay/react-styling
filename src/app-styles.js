deb06f02761c937914ddd669ec66f51e3dff6c79
Sidney <sofosu-ottopah@zaizi.com>
2019-03-21 14:57:27 +0000

    radium update

---
+++ src/app-styles.js
@@ -0,0 +1,12 @@
+export default {
+	'*': {
+		boxSizing: 'border-box'
+	},
+
+	html: {
+		fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+		fontSize: 14,
+		lineheight: '1.4em',
+		backgroundColor: '#1d272f'
+	}
+};
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import styles from './frame-styles.js'
-
 export default function Frame({ children }) {
-  return <div style={styles.root}>{children}</div>
+  return <div>{children}</div>
 }
